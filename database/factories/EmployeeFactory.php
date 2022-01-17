f83a5c1e047556d46e092cf2163a695ba494bd3e
AhmadRizal <amdrizalc@gmail.com>
2022-01-17 09:39:39 +0700

    add Employee Factory

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employee;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class EmployeeFactory extends Factory
@@ -11,10 +12,18 @@ class EmployeeFactory extends Factory
      *
      * @return array
      */
+    protected $model = Employee::class;
+
     public function definition()
     {
         return [
-            //
+            'emp_fullname' => $this->faker->name(),
+            'emp_email' => $this->faker->unique()->safeEmail,
+            'emp_address' => $this->faker->city(),
+            'emp_id' => $this->faker->randomNumber(9),
+            'emp_department' => $this->faker->randomElement(['Front-End', 'Back-End', 'Mobile Developer', 'Design']),
+            'gender' => $this->faker->randomElement(['Male', 'Female']),
+            'salary' => $this->faker->randomNumber(7),
         ];
     }
 }
